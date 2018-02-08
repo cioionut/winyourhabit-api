5df41c44a46201f2c727bb636f0bdd19352ab97f
Ionut Ciocoiu <ionutnciocoiu@gmail.com>
2018-02-08 20:46:58 +0200

    Add objective model to admin panel

--- winyourhabit_api/admin.py
+++ winyourhabit_api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from winyourhabit_api.models import User, HabitGroup, Proof
+from winyourhabit_api.models import User, HabitGroup, Proof, Objective
 
 admin.site.register(User, UserAdmin)
 
 admin.site.register(HabitGroup)
 admin.site.register(Proof)
+admin.site.register(Objective)
